749e7edada95725fdff77d08e910d3b8b35887f1
John Johnson-Rodgers <john@johnthe.dev>
2020-08-17 20:46:38 -0600

    using useState, what I'm used to, learned a little so far, definitely worth visiting further instruction.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
@@ -9,34 +9,31 @@ import Person from './Person/Person'
 //     </div>
 //   );
 // }
-class App extends Component {
-    state = {
-        persons:[
-            {name:'Johnny', age:31},
-            {name:'James', age:33},
-            {name:'Jared', age:29}]
-    }
-    changeNameHandler = () => {
-        console.log('was clicked');
-    }
-
-    render() {
+const App = props => {
+    const [personsState, setPersonsState] = useState({
+        persons: [
+            {name: 'John', age:'31'},
+            {name: 'Yvette', age:'31'},
+            {name: 'Jessica', age:'28'}
+        ],
+        otherState: 'Other value here'
+    });
         return (
             <div className='App'>
                 <h1>Hi I'm a React App</h1>
                 <button onClick={this.changeNameHandler}>Switch Name</button>
                 <p>This is working for shizzle.</p>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
+                <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>
                     <p>Hobbies: Racing</p>
                 </Person>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
                     <p>Hobbies: Running</p>
                 </Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+                <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>
                     <p>Hobbies: Boating</p>
                 </Person>
             </div>
         )
-          }
+
 }
 export default App;
